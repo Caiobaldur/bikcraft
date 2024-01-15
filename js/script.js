5050349165d9167502a3b89313ee23d8a80d67c2
Caio Balduino <caiobrafachino@hotmail.com>
2024-01-15 11:10:45 -0300

    [feat] gallery focus functionality

--- js/script.js
+++ js/script.js
@@ -34,8 +34,8 @@ function ativarPergunta(event) {
   const resposta = document.getElementById(controls);
 
   resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa")
-  console.log(ativa)
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
   pergunta.setAttribute("aria-expanded", ativa);
 }
 
@@ -44,3 +44,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
